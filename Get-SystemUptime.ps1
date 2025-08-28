9459966138df3f4d75e9bb11964cf40ef4ab871c
ThalleyCreek <thalleycreek@gmail.com>
2025-08-28 15:25:53 -0400

    Rename Since property to LastBootUpTime

--- Get-SystemUptime.ps1
+++ Get-SystemUptime.ps1
@@ -1,11 +1,21 @@
 function Get-SystemUptime {
+    <#
+    .SYNOPSIS
+        Returns system uptime information including the last boot time.
+
+    .EXAMPLE
+        PS> Get-SystemUptime
+
+        Days Hours Minutes LastBootUpTime
+        ---- ----- ------- --------------
+        5    3     20      10/19/2023 08:42:00
+    #>
     $OS = Get-CimInstance Win32_OperatingSystem
     $UpTime = (Get-Date) - $OS.LastBootUpTime
-
     [PSCustomObject]@{
         Days    = $UpTime.Days
         Hours   = $UpTime.Hours
         Minutes = $UpTime.Minutes
-        Since   = $OS.LastBootUpTime
+        LastBootUpTime = $OS.LastBootUpTime
     }
 }
